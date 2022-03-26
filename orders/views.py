f3e90ae4ea3844ce4ffe899e77efee1e29b8dc1f
suhailvs <suhailvs@gmail.com>
2022-03-26 14:07:33 +0530

    fetch delivery boys with in distance

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,8 @@ from distutils import dist
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
 from django.contrib.auth import get_user_model
+from django.db.models import Exists
+from django.db.models import OuterRef
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,6 +18,41 @@ from .models import Order
 class UserListAPIView(viewsets.ModelViewSet):
     queryset = get_user_model().objects.order_by('username')
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        """
+        Fetch Delivery boys with filters
+            a. By name
+            b. By mobile number
+        """
+        queryset = get_user_model().objects.filter(user_type=2)
+
+        # filter by name
+        name = self.request.query_params.get('name')
+        if name:
+            queryset=queryset.filter(first_name__icontains=name)
+            
+        # filter by mobile
+        mobile = self.request.query_params.get('mobile')
+        if mobile:
+            queryset = queryset.filter(mobile__icontains=mobile)
+        
+        # filter by location and radius
+        location = self.request.query_params.get('location')
+        if location:
+            latitude,longitude,km = location.split(',')
+
+            # radius in meters
+            radius = float(km)*1000 
+            user_location = Point(float(longitude), float(latitude), srid=4326)
+            queryset = queryset.filter(
+                Exists(Order.objects.filter(
+                    deliveryboy=OuterRef('pk')
+                ))
+            ).annotate(
+                distance=Distance('order__location', user_location)
+            ).filter(distance__lte=radius).order_by('distance')
+        return queryset
 
 class OrderView(viewsets.ModelViewSet):
     queryset = Order.objects.order_by('item')
@@ -61,10 +98,16 @@ class TakeOrderView(APIView):
 
     def post(self, request, format=None):
         order = Order.objects.get(id=request.data['orderid'])
-        msg = 'Order already assigned'
-        if not order.deliveryboy:
-            order.deliveryboy = request.user
-            order.save()
-            msg='Order succussfully assigned.'
+        if order.deliveryboy:
+            return Response('Order already assigned')
+        
+        if hasattr(request.user,'order') and request.user.order:
+            return Response('User already has order')
+        
+        order.deliveryboy = request.user
+        order.save()
+        return Response('Order succussfully assigned.')
 
-        return Response(msg)
+
+
+
